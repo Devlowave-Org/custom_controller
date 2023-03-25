b95529ccc384a94671af1bab22a62ade4fa18c53
GRISZ <griszou12@gmail.com>
2023-03-25 16:05:21 +0100

    refactored some code and make it work better

--- code/code.py
+++ code/code.py
@@ -1,24 +1,44 @@
-import time
 import board
-import buttons
+import time
 from send_key import *
+from digitalio import DigitalInOut, Direction, Pull
 
-# create the buttons
-up = buttons.Button(board.GP14)
-left = buttons.Button(board.GP10)
-down = buttons.Button(board.GP2)
-right = buttons.Button(board.GP27)
+# init the up button
+up_btn = DigitalInOut(board.GP4)
+up_btn.direction = Direction.INPUT
+up_btn.pull = Pull.DOWN
+
+# init the left button
+left_btn = DigitalInOut(board.GP8) 
+left_btn.direction = Direction.INPUT
+left_btn.pull = Pull.DOWN
+
+# init the down button
+down_btn = DigitalInOut(board.GP2)
+down_btn.direction = Direction.INPUT 
+down_btn.pull = Pull.DOWN
+
+# init the right button
+right_btn = DigitalInOut(board.GP6)
+right_btn.direction = Direction.INPUT
+right_btn.pull = Pull.DOWN
+
+# init the reset button
+reset_btn = DigitalInOut(board.GP10)
+reset_btn.direction = Direction.INPUT
+reset_btn.pull = Pull.DOWN
 
 while True:
-    if not up.btn.value:
+    if up_btn.value:
         send_key_z()
-    elif not left.btn.value:
+    elif left_btn.value:
         send_key_q()
-    elif not down.btn.value:
+    elif down_btn.value:
         send_key_s()
-    elif not right.btn.value:
+    elif right_btn.value:
         send_key_d()
+    elif reset_btn.value:
+        send_key_r()
     else:
-        pass
-    
-    time.sleep(0.1)
+        continue
+    time.sleep(0.3)
