d43deefc76b912996b5db231a695a2b404fd44a9
Grisz <griszou12@gmail.com>
2023-03-25 16:08:25 +0100

    deleted unused kay_shift var

--- code/send_key.py
+++ code/send_key.py
@@ -7,31 +7,29 @@ key_z = Keycode.W
 key_q = Keycode.A
 key_s = Keycode.S
 key_d = Keycode.D
-key_r = Keycode.R
-key_Shift = Keycode.SHIFT
 keyboard = Keyboard(usb_hid.devices)
 
 
 # the functions that send the keys
 def send_key_z():
-    keyboard.press(key_Shift, key_z)
-    keyboard.release(key_Shift, key_z)
+    keyboard.press(key_z)
+    keyboard.release(key_z)
 
 def send_key_q():
-    keyboard.press(key_Shift, key_q)
-    keyboard.release(key_Shift, key_q)
+    keyboard.press(key_q)
+    keyboard.release(key_q)
 
 def send_key_s():
-    keyboard.press(key_Shift, key_s)
-    keyboard.release(key_Shift, key_s)
+    keyboard.press(key_s)
+    keyboard.release(key_s)
 
 def send_key_d():
-    keyboard.press(key_Shift, key_d)
-    keyboard.release(key_Shift, key_d)
+    keyboard.press(key_d)
+    keyboard.release(key_d)
     
 def send_key_r():
-    keyboard.press(key_Shift, key_r)
-    keyboard.release(key_Shift, key_r)
+    keyboard.press(key_r)
+    keyboard.release(key_r)
 
 
 # this is for debugging :
